aa0611542366eae2eee72bc60d4e0aff57c1f461
Arjun Singha <singha.arjun@gmail.com>
2023-02-01 13:14:56 +0530

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geoCode = require('../utils/geocode.js')
-const forecast = require('../utils/forecast.js')
+const geoCode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
 
 const app = express()
